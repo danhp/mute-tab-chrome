efd63de23d3880bc812cd55ccabae0016162d31b
Daniel Pham <pham.dany@gmail.com>
2016-04-08 01:31:28 -0400

    Cleanup arrow functions

--- src/background.js
+++ src/background.js
@@ -1,19 +1,19 @@
-'use-strict'
+'use strict'
 let stateAllTabs = false;
 
-chrome.commands.onCommand.addListener((command) => {
+chrome.commands.onCommand.addListener(command => {
 	switch (command) {
 		case "mute_tab_current":
-			chrome.tabs.getSelected(null, (tab) => {
+			chrome.tabs.getSelected(null, tab => {
 				chrome.tabs.update(tab.id, {muted: !tab.mutedInfo.muted});
 			});
 			break;
 
 		case "mute_tab_all":
 			stateAllTabs = !stateAllTabs;
-			chrome.windows.getAll({populate: true}, (windowList) => {
-				windowList.forEach((window) => {
-					window.tabs.forEach((tab) => {
+			chrome.windows.getAll({populate: true}, windowList => {
+				windowList.forEach(window => {
+					window.tabs.forEach(tab => {
 						if (tab.audible || tab.mutedInfo.muted) {
 							chrome.tabs.update(tab.id, {muted: stateAllTabs});
 						}
@@ -23,16 +23,16 @@ chrome.commands.onCommand.addListener((command) => {
 			break;
 
 		case "mute_tab_all_except_current":
-			chrome.windows.getAll({populate: true}, (windowList) => {
-				windowList.forEach((window) => {
-					window.tabs.forEach((tab) => {
+			chrome.windows.getAll({populate: true}, windowList => {
+				windowList.forEach(window => {
+					window.tabs.forEach(tab => {
 						if (tab.audible) {
 							chrome.tabs.update(tab.id, {muted: true})
 						}
 					});
 				});
 			});
-			chrome.tabs.getSelected(null, (tab) => {
+			chrome.tabs.getSelected(null, tab => {
 				chrome.tabs.update(tab.id, {muted: false});
 			});
 			break;
