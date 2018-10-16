6d75aff6f692966d70d5470e9a5c8dabb2c7e321
gangadharjannu <gangadhar014@gmail.com>
2018-10-17 00:25:09 +0530

    Removed deprecated method and replaced with compatiable methods. Increased efficiency by using array filter method to filter only audible tabs

--- src/background.js
+++ src/background.js
@@ -4,39 +4,31 @@ let stateAllTabs = false;
 chrome.commands.onCommand.addListener(command => {
 	switch (command) {
 		case "mute_tab_current":
-			chrome.tabs.getSelected(null, tab => {
-				chrome.tabs.update(tab.id, {muted: !tab.mutedInfo.muted});
+			chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+				chrome.tabs.update(tabs[0].id, {muted: !tabs[0].mutedInfo.muted});
 			});
 			break;
 
 		case "mute_tab_all":
 			stateAllTabs = !stateAllTabs;
-			chrome.windows.getAll({populate: true}, windowList => {
-				windowList.forEach(window => {
-					window.tabs.forEach(tab => {
-						if (tab.audible || tab.mutedInfo.muted) {
-							chrome.tabs.update(tab.id, {muted: stateAllTabs});
-						}
-					});
+			chrome.tabs.query({},function(tabs){     
+				tabs.filter(tab=>tab.audible||tab.mutedInfo.muted).forEach(function(tab){
+					chrome.tabs.update(tab.id, {muted: stateAllTabs});
 				});
-			});
+			 });
 			break;
 
 		case "mute_tab_all_except_current":
-			chrome.windows.getAll({populate: true}, windowList => {
-				windowList.forEach(window => {
-					window.tabs.forEach(tab => {
-						if (tab.audible) {
-							chrome.tabs.update(tab.id, {muted: true})
-						}
-					});
+			chrome.tabs.query({},function(tabs){     
+				tabs.filter(tab=>tab.audible).forEach(function(tab){
+					chrome.tabs.update(tab.id, {muted: true});
 				});
 			});
-			chrome.tabs.getSelected(null, tab => {
-				chrome.tabs.update(tab.id, {muted: false});
+			chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+				chrome.tabs.update(tabs[0].id, {muted: false});
 			});
 			break;
-
+			
 		default:
 			break;
 	}
