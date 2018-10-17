ab2590adf5767b6999d1a7582cea4c5ddf6087ce
gangadharjannu <gangadhar014@gmail.com>
2018-10-17 22:51:43 +0530

    bumped version to 2.0.0 as there are breaking changes in API

--- src/background.js
+++ src/background.js
@@ -4,31 +4,32 @@ let stateAllTabs = false;
 chrome.commands.onCommand.addListener(command => {
 	switch (command) {
 		case "mute_tab_current":
-			chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-				chrome.tabs.update(tabs[0].id, {muted: !tabs[0].mutedInfo.muted});
+			chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([currentTab]) => {
+				chrome.tabs.update(currentTab.id, { muted: !currentTab.mutedInfo.muted });
 			});
 			break;
 
 		case "mute_tab_all":
 			stateAllTabs = !stateAllTabs;
-			chrome.tabs.query({},function(tabs){     
-				tabs.filter(tab=>tab.audible||tab.mutedInfo.muted).forEach(function(tab){
-					chrome.tabs.update(tab.id, {muted: stateAllTabs});
-				});
-			 });
+			chrome.tabs.query({}, tabs => {
+				tabs
+					.filter(tab => tab.audible || tab.mutedInfo.muted)
+					.forEach(tab => {
+						chrome.tabs.update(tab.id, { muted: stateAllTabs });
+					});
+			})
 			break;
 
 		case "mute_tab_all_except_current":
-			chrome.tabs.query({},function(tabs){     
-				tabs.filter(tab=>tab.audible).forEach(function(tab){
-					chrome.tabs.update(tab.id, {muted: true});
-				});
-			});
-			chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-				chrome.tabs.update(tabs[0].id, {muted: false});
+			chrome.tabs.query({}, tabs => {
+				tabs
+					.filter(tab => ((!tab.active) && tab.audible))
+					.forEach(tab => {
+						chrome.tabs.update(tab.id, { muted: true })
+					});
 			});
 			break;
-			
+
 		default:
 			break;
 	}
